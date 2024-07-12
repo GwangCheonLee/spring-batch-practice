1120fa8080b1949450f999e47c6e5bd9db3c986a
gclee <raitmid56@gmail.com>
2024-07-12 23:04:55 +0900

    refactor: Change queryDsl repository

--- src/main/java/com/example/springbatchpractice/another/repositories/AnotherRepository.java
+++ src/main/java/com/example/springbatchpractice/another/repositories/AnotherRepository.java
@@ -1,9 +1,31 @@
 package com.example.springbatchpractice.another.repositories;
 
 import com.example.springbatchpractice.another.entities.Another;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import static com.example.springbatchpractice.another.entities.QAnother.another;
+
 @Repository
-public interface AnotherRepository extends JpaRepository<Another, Long> {
+public class AnotherRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public AnotherRepository(@Qualifier("anotherJpaQueryFactory") JPAQueryFactory jpaQueryFactory) {
+        this.jpaQueryFactory = jpaQueryFactory;
+    }
+
+    public List<Another> findAllAnother() {
+        return jpaQueryFactory.select(Projections.bean(Another.class,
+                        another.id,
+                        another.name,
+                        another.email
+                ))
+                .from(another)
+                .fetch();
+    }
 }
