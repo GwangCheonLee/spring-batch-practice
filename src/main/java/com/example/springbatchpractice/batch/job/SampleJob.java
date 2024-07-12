ba1e9e738a79ea6868f9a9855a7c7e4ddca96903
gclee <raitmid56@gmail.com>
2024-07-12 23:18:41 +0900

    refactor: Batch file separate

---
+++ src/main/java/com/example/springbatchpractice/batch/job/SampleJob.java
@@ -0,0 +1,23 @@
+package com.example.springbatchpractice.batch.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class SampleJob {
+
+    @Bean("sampleJob")
+    public Job configureSampleJob(JobRepository jobRepository, Step sampleStep) {
+        return new JobBuilder("sampleJob", jobRepository)
+                .start(sampleStep)
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+}
