4a396b99989edfc07ed986c2a39ecda302425d86
gclee <raitmid56@gmail.com>
2024-07-12 22:48:14 +0900

    refactor: QuartzJob to streamline job parameter creation

--- src/main/java/com/example/springbatchpractice/quartz/job/QuartzJob.java
+++ src/main/java/com/example/springbatchpractice/quartz/job/QuartzJob.java
@@ -3,6 +3,7 @@ package com.example.springbatchpractice.quartz.job;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -11,25 +12,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class QuartzJob implements Job {
 
-    // Spring Batch 작업을 실행하기 위한 JobLauncher
     private final JobLauncher jobLauncher;
-    // 실행할 Spring Batch 작업
     private final org.springframework.batch.core.Job batchJob;
 
-    // 생성자를 통한 의존성 주입
     public QuartzJob(JobLauncher jobLauncher, @Qualifier("sampleJob") org.springframework.batch.core.Job batchJob) {
         this.jobLauncher = jobLauncher;
         this.batchJob = batchJob;
     }
 
-    // Quartz 스케줄러에 의해 실행될 메소드
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
+        JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+        jobParametersBuilder.addLong("uniqueness", System.nanoTime());
+        JobParameters jobParameter = jobParametersBuilder.toJobParameters();
+
         try {
-            // 고유성을 보장하기 위해 현재 시간을 기반으로 한 파라미터와 함께 작업 실행
-            jobLauncher.run(batchJob, new JobParametersBuilder().addLong("uniqueness", System.nanoTime()).toJobParameters());
+            jobLauncher.run(batchJob, jobParameter);
         } catch (Exception e) {
-            // 실행 중 예외 발생 시 JobExecutionException으로 포장하여 Quartz에 전달
             throw new JobExecutionException(e);
         }
     }
